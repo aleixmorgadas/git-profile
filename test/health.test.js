1e6767c7981914df8ae496ddde9eb01660da6efb
Aleix <aleixmorgadas@users.noreply.github.com>
2020-03-20 09:33:15 +0100

    added AUTHORS

--- test/health.test.js
+++ test/health.test.js
@@ -1,3 +1,9 @@
+/**
+ * Copyright (c) 2020 Contributors as noted in the AUTHORS file
+ * 
+ * This software is released under the MIT License.
+ * https://opensource.org/licenses/MIT
+ */
 test("Health test", () => {
     expect(true).toBe(true);
 });
